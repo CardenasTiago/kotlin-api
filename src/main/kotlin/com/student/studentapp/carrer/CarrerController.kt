ae7f25ad3bfa889ebf9d75a664242d1a37172c2f
CardenasTiago <cardenas.tiago.r@gmail.com>
2025-05-06 01:43:49 -0300

    Se realizo el crud de carrer

--- src/main/kotlin/com/student/studentapp/carrer/CarrerController.kt
+++ src/main/kotlin/com/student/studentapp/carrer/CarrerController.kt
@@ -1,4 +1,24 @@
 package com.student.studentapp.carrer
 
-class CarrerController {
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/carrers")
+class CarrerController(private val carrerService: CarrerService) {
+
+    @PostMapping
+    fun create(@RequestBody carrer: Carrer) = carrerService.create(carrer)
+
+    @GetMapping
+    fun list() = carrerService.list()
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable id: Long): Carrer{
+        return carrerService.getById(id)
+    }
+
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable id: Long){
+        return carrerService.delete(id)
+    }
 }
--- src/main/kotlin/com/student/studentapp/carrer/CarrerService.kt
+++ src/main/kotlin/com/student/studentapp/carrer/CarrerService.kt
@@ -2,6 +2,18 @@ package com.student.studentapp.carrer
 
 import org.springframework.stereotype.Service
 
+@Service
 class CarrerService(private val carrerRepository: CarrerRepository) {
-    
+    fun create(carrer: Carrer): Carrer = carrerRepository.save(carrer)
+    fun list():List<Carrer> = carrerRepository.findAll()
+    fun getById(id : Long): Carrer{
+        return carrerRepository.findById(id).orElseThrow{
+            NoSuchElementException("Carrera no encontrada")
+        }
+    }
+
+    fun delete(id: Long){
+        return carrerRepository.deleteById(id)
+    }
+
 }
